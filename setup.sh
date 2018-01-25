2d24e39a0c7e75b6127de67afa31c27e0d3730be
Sander van Melsen <sandervanmelsen@gmail.com>
2018-01-25 22:17:09 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# https://github.com/alrra/dotfiles/blob/master/src/os/utils.sh
+CURRENT_DIR=$(pwd)
+BACKUP_DIR="$HOME/.dotfiles/backup/"
 
-CWD=$(pwd)
-
-backup_dir="$HOME/.dotfiles/backup/"
-
-apt_packages=(
+declare -a apt_packages=(
   "git"
   "docker.io"
   "docker-compose"
@@ -16,9 +13,9 @@ apt_packages=(
   "python-dev"
   "virtualenv"
   "build-essential"
-  "libssl-dev"
   "gnome-tweak-tool"
   "numix-gtk-theme"
+  "libssl-dev"
   "libc6-dev"
   "libncursesw5-dev"
   "libgdbm-dev"
@@ -27,48 +24,50 @@ apt_packages=(
   "zlib1g-dev"
 )
 
-symlink_dotfiles_source=(
-  "$CWD/terminator/config"
-  "$CWD/git/.gitconfig"
+declare -a symlink_dotfiles_source=(
+  "$CURRENT_DIR/terminator/config"
+  "$CURRENT_DIR/git/.gitconfig"
 )
 
-symlink_dotfiles_dest=(
-  # "$HOME/.config/terminator/config"
-  # "$HOME/.gitconfig"
-  "/tmp/test1"
-  "/tmp/test2"
+declare -a symlink_dotfiles_dest=(
+  "$HOME/.config/terminator/config"
+  "$HOME/.gitconfig"
+)
+
+declare -a ubuntu_appearance_gsetting=(
+  "org.gnome.desktop.interface gtk-theme 'Numix'"
+  "org.gnome.desktop.interface clock-show-date true"
+  "org.gnome.desktop.calendar show-weekdate true"
+  "org.gnome.nautilus.desktop home-icon-visible false"
+  "org.gnome.nautilus.desktop volumes-visible false"
+  "org.gnome.nautilus.desktop network-icon-visible false"
+  "org.gnome.nautilus.desktop trash-icon-visible false"
 )
 
 print_error() {
-  # Print output in red
   printf "\e[0;31m  [✖] $1 $2\e[0m\n"
 }
 
 print_info() {
-  # Print output in purple
   printf "\n\e[0;35m $1\e[0m\n"
 }
 
 print_question() {
-  # Print output in yellow
-  printf "\e[0;33m [?] $1\e[0m"
+  printf "\n\e[0;33m [?] $1\e[0m"
 }
 
 print_success() {
-  # Print output in green
   printf "\e[0;32m [✔] $1\e[0m\n"
 }
 
 print_result() {
-  [ $1 -eq 0 ] && print_success "$2" || print_error "Failed"
-  # [ "$3" == "true" ] && [ $1 -ne 0 ] && exit
+  [ $1 -eq 0 ] && print_success "${2:-Success}" || print_error "${3:-Failed}"
 }
 
 ask_for_sudo() {
   sudo -v &> /dev/null
 
-  # Update existing `sudo` time stamp until this script has finished
-  # https://gist.github.com/cowboy/3118588
+  # Update existing sudo timestamp until this script has finished
   while true; do
     sudo -n true
     sleep 60
@@ -80,12 +79,12 @@ mkd() {
   if [ -n "$1" ]; then
     if [ -e "$1" ]; then
       if [ ! -d "$1" ]; then
-        print_error "$1 - a file with the same name already exists!"
+        print_error "File with the same name already exists! ($1)"
       else
         print_success "$1"
       fi
     else
-      execute "mkdir -p $1" "$1"
+      execute "mkdir -p $1"
     fi
   fi
 }
@@ -93,42 +92,51 @@ mkd() {
 execute() {
   print_info "Execute: $1"
   $1 &> /dev/null
-  print_result $? "${2:-$1}"
+  print_result $? $2 $3
 }
 
 install_apt_packages() {
-  execute "sudo apt-get update" "Success"
-  execute "sudo apt-get upgrade" "Success"
-  execute "sudo apt-get install --yes ${apt_packages[*]}" "Success"
+  print_info "Installing packages (can take a moment)"
+  execute "sudo apt-get update"
+  execute "sudo apt-get upgrade --yes"
+  execute "sudo apt-get install --yes ${apt_packages[*]}"
 }
 
 backup_dotfiles() {
   mkd $backup_dir
 
   for i in ${symlink_dotfiles_source[@]}; do
-    execute "cp --parents $i $backup_dir" "Success"
+    execute "cp --parents $i $BACKUP_DIR"
   done
 }
 
 symlink_dotfiles() {
-  # loop over all the keys of the array
   for i in ${!symlink_dotfiles_source[@]}; do
-    execute "ln --symbolic --force ${symlink_dotfiles_source[$i]} ${symlink_dotfiles_dest[$i]}" "Success"
+    execute "ln --symbolic --force ${symlink_dotfiles_source[$i]} ${symlink_dotfiles_dest[$i]}"
+  done
+}
+
+ubuntu_appearance() {
+  for i in ${!ubuntu_appearance_gsetting[@]}; do
+    execute "gsettings set ${ubuntu_appearance_gsetting[$i]}"
   done
 }
 
 confirm() {
   while true; do
-    print_question "$1 [y/n]"
-    read -p " " yn
+    print_question "$1 [Y/n]"
+    read -p " " input
 
-    case $yn in
+    case $input in
       [Yy]* )
         $2
-        break
-        ;;
-      [Nn]* ) break;;
-      * ) echo "Please answer yes or no.";;
+        break;;
+      [Nn]* )
+        break;;
+      * )
+        $2
+        break;;
+      # * ) echo "Please answer yes or no.";;
     esac
   done
 }
@@ -137,10 +145,9 @@ main() {
   ask_for_sudo
 
   confirm "Install apt packages?" install_apt_packages
-
   confirm "Backup original dotfiles?" backup_dotfiles
-
   confirm "Symlink dotfiles?" symlink_dotfiles
+  confirm "Set appearances?" ubuntu_appearance
 }
 
 main
